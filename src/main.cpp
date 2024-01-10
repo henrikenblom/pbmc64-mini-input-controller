bc322ecfe3c94ecc143f9f448b0daf691ec5cd52
Henrik Enblom <henblom@spotify.com>
2024-01-10 23:47:47 +0100

    Copy over joystick code from the wireless receiver implementation

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,44 @@
 
 #define NUM_LEDS 1
 #define TYPING_DELAY 70
+#define LOCAL_JOYSTICK_FIRE_PIN 0
+#define LOCAL_JOYSTICK_PROGRAMMABLE_BUTTON_PIN 1
+#define AUX_BUTTON_PIN 11
+#define LOCAL_JOYSTICK_X_PIN PIN_A3
+#define LOCAL_JOYSTICK_Y_PIN PIN_A4
+#define LOCAL_JOYSTICK_RIGHT_TRIGGER  275
+#define LOCAL_JOYSTICK_LEFT_TRIGGER 470
+#define LOCAL_JOYSTICK_UP_TRIGGER 450
+#define LOCAL_JOYSTICK_DOWN_TRIGGER 230
 
 CRGB leds[NUM_LEDS];
+PBMC64Wireless::JoystickInput currentLocalJoystickInput;
+KeyboardKeycode localJoystickAuxKey = KEY_SPACE;
+bool localJoystickAuxKeyNeedsRelease = false;
+bool auxButtonPressed = false;
+bool programmingHappened = false;
 
 void setupUSBKeyboard() {
   BootKeyboard.begin();
   BootKeyboard.releaseAll();
+}
+
+void clearLocalJoystick() {
+  currentLocalJoystickInput.fire = false;
+  currentLocalJoystickInput.autofire = false;
+  currentLocalJoystickInput.left = false;
+  currentLocalJoystickInput.right = false;
+  currentLocalJoystickInput.up = false;
+  currentLocalJoystickInput.down = false;
+}
+
+void initLocalInput() {
+  pinMode(LOCAL_JOYSTICK_PROGRAMMABLE_BUTTON_PIN, INPUT);
+  pinMode(LOCAL_JOYSTICK_FIRE_PIN, INPUT);
+  pinMode(LOCAL_JOYSTICK_X_PIN, INPUT);
+  pinMode(LOCAL_JOYSTICK_Y_PIN, INPUT);
+  pinMode(AUX_BUTTON_PIN, INPUT);
+  clearLocalJoystick();
 }
 
 void setup() {
@@ -29,5 +61,84 @@ void type(KeyboardKeycode k) {
   delay(TYPING_DELAY);
 }
 
+size_t removeKey(KeyboardKeycode keycode) {
+  if (currentLocalJoystickInput.autofire) {
+    if (localJoystickAuxKey != keycode) {
+      return BootKeyboard.remove(keycode);
+    } else {
+      return 0;
+    }
+  }
+  return BootKeyboard.remove(keycode);
+}
+
+size_t addKey(KeyboardKeycode keycode) {
+  if (auxButtonPressed) {
+    localJoystickAuxKey = keycode;
+    BootKeyboard.remove(keycode);
+    programmingHappened = true;
+  }
+  return BootKeyboard.add(keycode);
+}
+
+bool localJoystickActive() {
+  return currentLocalJoystickInput.left
+         || currentLocalJoystickInput.right
+         || currentLocalJoystickInput.up
+         || currentLocalJoystickInput.down
+         || currentLocalJoystickInput.fire
+         || currentLocalJoystickInput.autofire;
+}
+
+void updateLocalJoystickState() {
+  if (localJoystickActive()) {
+    if (currentLocalJoystickInput.autofire) {
+      BootKeyboard.add(localJoystickAuxKey);
+      localJoystickAuxKeyNeedsRelease = true;
+    } else {
+      BootKeyboard.remove(localJoystickAuxKey);
+    }
+    currentLocalJoystickInput.left ? addKey(KEYPAD_7) : removeKey(KEYPAD_7);
+    currentLocalJoystickInput.right ? addKey(KEYPAD_1) : removeKey(KEYPAD_1);
+    currentLocalJoystickInput.up ? addKey(KEYPAD_9) : removeKey(KEYPAD_9);
+    currentLocalJoystickInput.down ? addKey(KEYPAD_3) : removeKey(KEYPAD_3);
+    currentLocalJoystickInput.fire ? addKey(KEYPAD_0) : removeKey(KEYPAD_0);
+  } else {
+    BootKeyboard.release(KEYPAD_7);
+    BootKeyboard.release(KEYPAD_1);
+    BootKeyboard.release(KEYPAD_9);
+    BootKeyboard.release(KEYPAD_3);
+    BootKeyboard.release(KEYPAD_0);
+    if (localJoystickAuxKeyNeedsRelease) {
+      BootKeyboard.release(localJoystickAuxKey);
+      localJoystickAuxKeyNeedsRelease = false;
+    }
+  }
+}
+
+void pollLocalJoystick() {
+  clearLocalJoystick();
+  int x = (int) analogRead(LOCAL_JOYSTICK_X_PIN);
+  int y = (int) analogRead(LOCAL_JOYSTICK_Y_PIN);
+  currentLocalJoystickInput.autofire = digitalRead(LOCAL_JOYSTICK_PROGRAMMABLE_BUTTON_PIN);
+  currentLocalJoystickInput.fire = digitalRead(LOCAL_JOYSTICK_FIRE_PIN);
+  if (x < LOCAL_JOYSTICK_RIGHT_TRIGGER) {
+    currentLocalJoystickInput.right = true;
+  } else if (x > LOCAL_JOYSTICK_LEFT_TRIGGER) {
+    currentLocalJoystickInput.left = true;
+  }
+  if (y > LOCAL_JOYSTICK_UP_TRIGGER) {
+    currentLocalJoystickInput.up = true;
+  } else if (y < LOCAL_JOYSTICK_DOWN_TRIGGER) {
+    currentLocalJoystickInput.down = true;
+  }
+}
+
 void loop() {
+  //TODO: Add hardware and uncomment:
+  /*pollLocalJoystick();
+  updateLocalJoystickState();
+  if (localJoystickActive()) {
+    BootKeyboard.send();
+  }*/
 }
